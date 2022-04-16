c1dbc866f1b459436a54f9a8d82d95e4b828dfb4
unknown <girish.krs@gmail.com>
2022-04-16 11:35:29 +0530

    modified folder structure

--- src/test/Runner/MyRunner/TestRunner.java
+++ src/test/Runner/MyRunner/TestRunner.java
@@ -1,27 +1,51 @@
 package MyRunner;
 
+import java.io.File;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+//import com.cucumber.listener.Reporter;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
+import managers.FileReaderManager;
+//import managers.FileReaderManager;
 
 	@RunWith(Cucumber.class)
+	
 		@CucumberOptions(
 				//features = "E:/MarketOne -V1/src/main/java/Features/login.feature", //the path of the feature files
 				features = "classpath:Features/login.feature",
 				
+						//E:\MarketOne -V1\src\test\resources\Features\login.feature
+						///MarketOnce/src/test/resources/Features/login.feature
+				// classpath
 				glue={"stepDefinitions"}, //the path of the step definition files
 				//format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
+				//plugin = {"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
+						//tags = {"@Login"},
+				plugin = { "pretty","html:target/cucumber-reports.html", "json:target/cucumber.json", "junit:target/cucumber.xml" },
 				monochrome = true, //display the console output in a proper readable format
 				strict = true, //it will check if any step is not defined in step definition file
-				dryRun = false, //to check the mapping igherkins proper between feature file and step def file
+				
+				
+				dryRun = false //to check the mapping igherkins proper between feature file and step def file
 				//tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}			
-						//tags = {@Login}
-				plugin = { "pretty","html:target/cucumber-reports.html", "json:target/cucumber.json", "junit:target/cucumber.xml" }
+						
+			//	plugin = { "pretty","html:target/cucumber-reports.html", "json:target/cucumber.json", "junit:target/cucumber.xml","com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html" }
+			
 				)
 	 
 	public class TestRunner {
+		
+		// adding method to write the report.
+		
+		@AfterClass
+		public static void writeExtentReport() {
+			//Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
+			//Reporter.loadXMLConfig(new File("E:/MarketOne -V1/configs/extent-config.xml"));
+		}
 		
 	}
 	
--- src/test/resources/Features/login.feature
+++ src/test/resources/Features/login.feature
@@ -17,6 +17,23 @@ Then Close the browser
  
 
 
+
+
+
+#without Examples Keyword
+@Login1 
+Scenario: MarketOnce Bid Search Test Scenario1
+
+Given user is already on Login Page
+When title of login page is MarketOnce
+Then user enters "automation.qa@roirocket.com" and "demopassword123"
+Then user clicks on login button
+Then user is on home page
+Then user clicks on Bids 
+Then user validate Projects By searching project ID "2370"
+Then Logout the Application
+Then Close the browser
+
 #with Examples Keyword
 #Scenario Outline: Free Marketonce Login Test Scenario
 
